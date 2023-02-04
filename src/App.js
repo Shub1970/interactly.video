cd09681557c790c685799671879853a6618cb01e
Shub1970 <18shubham1998@gmail.com>
2023-02-04 09:11:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,66 @@
+import React from "react";
 import "./App.css";
 import MyVideo from "./component/MyVideo";
 import { useEffect, useState } from "react";
 import wel from "./video/welcome.mp4";
 import thak from "./video/thankyou.mp4";
 import web from "./video/my-web.mp4";
+
 function App() {
   const [vid, setVid] = useState(null);
-  // const [display, setDisplay] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const handleSubmit = (event, video) => {
+    event.preventDefault();
+    setVid(video);
+  };
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
   useEffect(() => {
     setVid(wel);
   }, []);
+
   return (
     <div className="App">
       <div className="left">
         <MyVideo videoSource={vid} />
       </div>
       <div className="right">
-        <div className="btn">
-          <button className="Opetion-a" onClick={() => setVid(wel)}>
+        <form>
+          <button
+            className={`option-button ${selectedOption === "A" && "selected"}`}
+            onClick={(event) => {
+              handleSubmit(event, wel);
+              handleOptionClick("A");
+            }}
+            type="submit"
+          >
             <span className="Letter">A</span>
             <span className="Content">Welcome</span>
           </button>
           <button
-            className="Opetion-b"
-            onClick={() => {
-              setVid(web);
-              // setDisplay(true);
+            className={`option-button ${selectedOption === "B" && "selected"}`}
+            onClick={(event) => {
+              handleSubmit(event, web);
+              handleOptionClick("B");
             }}
+            type="submit"
           >
             <span className="Letter">B</span>
-            <span className="Content">visit my website</span>
+            <span className="Content">Visit my website</span>
           </button>
-          <button className="Opetion-c" onClick={() => setVid(thak)}>
+          <button
+            className={`option-button ${selectedOption === "C" && "selected"}`}
+            onClick={(event) => {
+              handleSubmit(event, thak);
+              handleOptionClick("C");
+            }}
+            type="submit"
+          >
             <span className="Letter">C</span>
-            <span className="Content">Thank You</span>
+            <span className="Content">Thank you</span>
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
